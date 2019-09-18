4b8a5081a51879ef7c2ebe21bb546a200dd45225
preritk <preritkhandalwal@gmail.com>
2019-09-18 23:51:49 +0530

    Animations added

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,41 +7,42 @@ class Contact extends Component{
             <div className="contact-body">
             <Grid className="contact-grid">
                 <Cell cols={6}>
-                    <h2 style={{color:'white'}}>PRERIT KHANDELWAL</h2>
+                    <h2 style={{color:'white'}} class="animated fadeInLeftBig">PRERIT KHANDELWAL</h2>
                     <img src="https://d39qdlcrvnra4b.cloudfront.net/avatars/1605846/original/avatar.png?1559039487"
                     alt="avatar-pic"
                     style={{height:'250px'}}
+                    class="animated fadeInLeftBig"
                     />
-                    <p style={{width:'75%', margin:'auto', paddingTop:'2em',color:'white',fontSize:'14px'}}>Thank you for visiting my site!<br/>Let's know each other more.<br/> Contact me on the given contact numbers or drop an email for me at preritkhandalwal@gmail.com.<br/>I'll be happy to hear from you!</p>
+                    <p class="animated fadeInLeftBig" style={{width:'75%', margin:'auto', paddingTop:'2em',color:'white',fontSize:'14px'}}>Thank you for visiting my site!<br/>Let's know each other more.<br/> Contact me on the given contact numbers or drop an email for me at preritkhandalwal@gmail.com.<br/>I'll be happy to hear from you!</p>
                 </Cell>
                 <div className="vertical-line"/>
                 <Cell cols={6}>
                 <div style={{paddingLeft:'3rem'}}>
-                <h2 style={{color:'white'}}>CONTACT ME</h2>
+                <h2 style={{color:'white'}} class="animated fadeInRightBig">CONTACT ME</h2>
                 <hr />
                 </div>
                 <div className="contact-list">
                 <List>
                     <ListItem>
-                        <ListItemContent style={{fontFamily:'Anton',fontSize:'25px',color:'white'}}>
+                        <ListItemContent style={{fontFamily:'Anton',fontSize:'25px',color:'white'}} class="animated slideInRight delay-3s">
                             <i className="fa fa-phone-square" aria-hidden='true'/>
-                              (+91) - 94XXXXXXXX
+                            (+91) - 94XXXXXXXX
                         </ListItemContent>
                     </ListItem>
                     <ListItem>
-                        <ListItemContent style={{fontFamily:'Anton',fontSize:'25px',color:'white'}}>
+                        <ListItemContent style={{fontFamily:'Anton',fontSize:'25px',color:'white'}} class="animated slideInRight delay-3s">
                             <i className="fa fa-phone-square" aria-hidden='true'/>
                              (+91) - 63XXXXXXXXX
                         </ListItemContent>
                     </ListItem>
                     <ListItem>
-                        <ListItemContent style={{fontFamily:'Anton',fontSize:'25px',color:'white'}}>
+                        <ListItemContent style={{fontFamily:'Anton',fontSize:'25px',color:'white'}} class="animated slideInRight delay-3s">
                             <i className="fa fa-home" aria-hidden='true'/>
                              (0141) - 2338905
                         </ListItemContent>
                     </ListItem>
                     <ListItem>
-                        <ListItemContent style={{fontFamily:'Anton',fontSize:'25px',color:'white'}}>
+                        <ListItemContent style={{fontFamily:'Anton',fontSize:'25px',color:'white'}} class="animated slideInRight delay-3s">
                             <i className="fa fa-skype" aria-hidden='true'/>
                                 preritkhandalwal 
                         </ListItemContent>
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -6,15 +6,16 @@ class LandingPage extends Component{
             <div style = {{width:'100%' ,margin:'auto'}}>
                 <Grid className = "landing-grid">
                     <Cell cols={12}>
-                        <img src="https://d39qdlcrvnra4b.cloudfront.net/avatars/1605846/original/avatar.png?1559039487" 
+                        <div class="animated slideInUp"><img src="https://d39qdlcrvnra4b.cloudfront.net/avatars/1605846/original/avatar.png?1559039487" 
                         alt="avatar-pic"
-                        className="avatar-img">
-                        </img>
+                        className="avatar-img" 
+                        >
+                        </img></div>
                         <div className="banner-text">
-                            <h1>I'm a Full Stack Developer</h1>
+                            <h1 class="animated slideInUp delay-2s">I'm a Full Stack Developer</h1>
                             <hr/>
-                            <p>Machine Learning | React Js | C/C++ | Python| MySQL | Competitive Programming</p>
-                            <div className="social-links">
+                            <p class="animated slideInUp delay-2s">Machine Learning | React Js | C/C++ | Python| MySQL | Competitive Programming</p>
+                            <div class="animated slideInUp"><div className="social-links">
                                 {/* LinkedIn*/}
                                 <a href="https://www.linkedin.com/in/prerit-khandelwal-411a31156/" rel="noopener noreferrer" target="_blank">
                                     <i className="fa fa-linkedin-square" aria-hidden='true'/>
@@ -31,6 +32,7 @@ class LandingPage extends Component{
                                 <a href="https://www.instagram.com/preritkhandalwal/" rel="noopener noreferrer" target="_blank">
                                     <i className="fa fa-instagram" aria-hidden='true'/>
                                 </a>
+                            </div>
                             </div>
                         </div>
                     </Cell>
